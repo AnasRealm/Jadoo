dc898bf6dca7c9466ea675d205010341023a44a2
Anas Alholo <anas.alholo.2005@gmail.com>
2025-10-21 21:58:10 +0300

    Initial commit - Jadoo Travel Website

---
+++ main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".testimonial-swiper", {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  effect: "slide",
+  speed: 600,
+});
